da7c7eca19d7ebacf93cd6194c58fbd45de873eb
Pei-Mei Lee <149309514+PeiMeiLee@users.noreply.github.com>
2023-10-30 18:18:24 +0100

    Updated number_of_customers_per_country.sql

--- 03 Scripts/number_of_customers_per_country.sql
+++ 03 Scripts/number_of_customers_per_country.sql
@@ -1,8 +1,7 @@
 -- Calculate number of customers in each country
 
-SELECT 
-D.country,
-COUNT(DISTINCT customer_id) AS count_customers
+SELECT D.country,
+  COUNT(DISTINCT customer_id) AS count_customers
 FROM customer A
 INNER JOIN address B ON A.address_id = B.address_id
 INNER JOIN city C ON B.city_id = C.city_id
