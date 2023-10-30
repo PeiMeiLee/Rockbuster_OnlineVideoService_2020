7141014823bc038ada726e6ad1402715c5b4d68c
Pei-Mei Lee <149309514+PeiMeiLee@users.noreply.github.com>
2023-10-30 18:31:43 +0100

    Updated top10_cities_within_top10_countries.sql

--- 03 Scripts/top10_cities_within_top10_countries.sql
+++ 03 Scripts/top10_cities_within_top10_countries.sql
@@ -1,8 +1,8 @@
 --  Top 10 cities that fall within any of the top 10 countries identified in previous step
 
 SELECT D.country,
-C.city,
-COUNT(customer_id) AS count_customers
+    C.city,
+    COUNT(customer_id) AS count_customers
 FROM customer A
 INNER JOIN address B ON A.address_id = B.address_id
 INNER JOIN city C ON B.city_id = C.city_id
